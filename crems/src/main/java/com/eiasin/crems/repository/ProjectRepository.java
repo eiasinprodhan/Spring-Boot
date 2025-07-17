a6dbfe24019ca180c5d2de4f80ace1919a457905
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-07-17 18:46:02 +0600

    Removed

--- crems/src/main/java/com/eiasin/crems/repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eiasin.crems.repository;
-
-import com.eiasin.crems.entity.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProjectRepository extends JpaRepository<Project, Integer> {
-}
--- project/src/main/java/com/emranhss/project/repository/PoliceStationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.emranhss.project.repository;
-
-import com.emranhss.project.entity.PoliceStation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PoliceStationRepository extends JpaRepository<PoliceStation, Integer> {
-}
