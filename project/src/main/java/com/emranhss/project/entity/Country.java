29b028d71b509d518386f196b2804caaee408508
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-07-24 00:28:59 +0600

    Update Country.java

--- project/src/main/java/com/emranhss/project/entity/Country.java
+++ project/src/main/java/com/emranhss/project/entity/Country.java
@@ -19,12 +19,27 @@ public class Country {
     private List<Division> divisions;
 
     // Getters & Setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public int getId() {
+        return id;
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-    public List<Division> getDivisions() { return divisions; }
-    public void setDivisions(List<Division> divisions) { this.divisions = divisions; }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Division> getDivisions() {
+        return divisions;
+    }
+
+    public void setDivisions(List<Division> divisions) {
+        this.divisions = divisions;
+    }
 }
