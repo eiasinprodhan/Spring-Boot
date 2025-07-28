13b568a4f282a56ce8cdf96e5e6cb6f5893f62ab
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-07-28 19:03:19 +0600

    Added

--- crud/src/main/java/com/thymeleaf/crud/CrudApplication.java
+++ crud/src/main/java/com/thymeleaf/crud/CrudApplication.java
@@ -1,13 +1,13 @@
-package com.emranhss.project;
+package com.thymeleaf.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Project64Application {
+public class CrudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Project64Application.class, args);
+		SpringApplication.run(CrudApplication.class, args);
 	}
 
 }
